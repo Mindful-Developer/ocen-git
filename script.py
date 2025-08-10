ba42420d339265afbb11a976ecda4b760abaf663
Christopher Busse <mindful.d3v@gmail.com>
2025-08-10 15:42:14 -0400

    add reading and writing .git/index file

--- script.py
+++ script.py
@@ -1,7 +1,10 @@
 import zlib
+import pprint
+from dataclasses import dataclass
+
+
 obj = "9c612ec60497146e46229bc2ce24daedf3f9e3ae"
 with open(f"test/.git/objects/{obj[:2]}/{obj[2:]}", 'rb') as fb:
     contents = zlib.decompress(fb.read())
 
 print(contents)
-
